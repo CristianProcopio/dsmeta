793217215033bc5e72a6138241125d1120e77c66
Cristian Procopio <cristianpteixeira@gmail.com>
2022-07-11 16:13:49 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-function App() {
-    return(
-      <h1>ola mundo</h1>
-)
+import NotificationButton from './components/NotificationButton'
 
+function App() {
+  return (
+    <>
+      <h1>ola mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
