2634675279bbe47cfc3bfeb7e3e420cedf75f12c
Cristian Procopio <cristianpteixeira@gmail.com>
2022-07-16 15:41:52 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from 'axios'
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../Utils/request'
 import './style.css'
 
-function NotificationButton() {
+type Props = {
+  saleId: number
+}
+
+function hendleClik(id: number) {
+  axios(`${BASE_URL}/Sales/${id}/Notification`).then(response => {
+    console.log('SUCESSO')
+  })
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
     <>
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={() => hendleClik(saleId)}>
         <img src={icon} alt="Notificar" />
       </div>
     </>
